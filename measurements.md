472d686c4ab186012b8e4a9d14afc00076357b21
Peter Sturm <sturm@uni-trier.de>
2023-11-23 13:39:56 +0100

    AtomicInteger
    … successfully avoids any data races.

--- measurements.md
+++ measurements.md
@@ -1,3 +1,4 @@
 # Mac Pro (3,5 GHz 6-Core Intel Xeon E5, 32 GB 1866 MHz DDR3)
 
 - With data races (10 * 10^7): Has data races, 11.406ms
+- Atomic Integer (10 * 10^7): No data races, 266.254ms
--- src/NoDataRaces.java
+++ src/NoDataRaces.java
@@ -1,12 +1,14 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class NoDataRaces {
     private static final int n_threads = 10;
     private static final int count_to = 10000000;
 
-    private static int counter = 0;
+    private static AtomicInteger counter = new AtomicInteger(0);
 
     private static void counting ( int up_to ) {
         for (int i=0; i<up_to; i++)
-            counter += 1;
+            counter.getAndIncrement();
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -29,7 +31,7 @@ public class NoDataRaces {
         double duration_in_msec = (((double) stop) - ((double) start)) / 10e6;
 
         System.out.printf("counter should be %d\n",n_threads*count_to);
-        System.out.printf("counter is %d\n",counter);
+        System.out.printf("counter is %d\n",counter.get());
         System.out.printf("Execution time %.4f msec\n",duration_in_msec);
     }
 }
